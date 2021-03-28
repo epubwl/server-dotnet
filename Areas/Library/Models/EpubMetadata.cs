d6294ea7b7cd6d8b08b22c63fe4b46be36a88df4
jayruin <28711665+jayruin@users.noreply.github.com>
2021-03-28 10:50:17 -0400

    Update EpubMetadata to match all dc elements.

--- Areas/Library/Models/EpubMetadata.cs
+++ Areas/Library/Models/EpubMetadata.cs
@@ -8,20 +8,34 @@ namespace EpubWebLibraryServer.Areas.Library.Models
 
         public string Owner { get; set; }
 
-        public string Identifier { get; set; }
+        public string Contributors { get; set; }
 
-        public string Title { get; set; }
+        public string Coverage { get; set; }
 
         public string Creators { get; set; }
 
-        public string Contributors { get; set; }
-
-        public string Publisher { get; set; }
-
         public DateTime? Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string Format { get; set; }
+
+        public string Identifier { get; set; }
 
         public string Languages { get; set; }
 
-        public string Description { get; set; }
+        public string Publisher { get; set; }
+        
+        public string Relation { get; set; }
+
+        public string Rights { get; set; }
+
+        public string Source { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Title { get; set; }
+
+        public string Type { get; set; }
     }
 }
