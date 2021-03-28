277e2389e4e710f7a9d2ce95eed0145a7d8de842
jayruin <28711665+jayruin@users.noreply.github.com>
2021-03-28 10:32:48 -0400

    Change all login error responses to be 400.

--- Areas/User/Controllers/AuthenticationController.cs
+++ Areas/User/Controllers/AuthenticationController.cs
@@ -49,7 +49,7 @@ namespace EpubWebLibraryServer.Areas.User.Controllers
             PasswordVerificationResult result = _userManager.PasswordHasher.VerifyHashedPassword(user, user.PasswordHash, userCredentials.Password);
             if (result != PasswordVerificationResult.Success)
             {
-                return Unauthorized();
+                return BadRequest();
             }
             return Ok(new { token = _tokenGenerator.GenerateToken(user) });
         }
